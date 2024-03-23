e9245e5126258d75175a24f99294e07c261b43cc
jincabia <140627796+jincabia@users.noreply.github.com>
2024-03-23 17:06:06 -0600

    finsihed budget

--- App.jsx
+++ App.jsx
@@ -8,11 +8,13 @@ import {
   Text,
   useColorScheme,
   View,
-  Image
+  Image,
+  Alert
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 
 
@@ -21,9 +23,12 @@ import BudgetTab from './src/tabs/BudgetTab';
 import ExpenseTab from './src/tabs/ExpenseTab';
 import SettingsTab from './src/tabs/SettingsTab';
 
+import AddBudgetTab from './src/tabs/AddBudgetTab';
+
 
 const Tab = createBottomTabNavigator();
-// const Stack = createStackNavigator();
+
+
 
 
 
@@ -39,11 +44,6 @@ import {
 export default function App()
 {
 
-
-  const getRandomColor = () => {
-
-    return "#" + Math.floor(Math.random() * 16777215).toString(16);
-   }
   
   const [budget, setBudgets] = React.useState(
     [
@@ -57,6 +57,8 @@ export default function App()
   const handleAddBudget = (newbudget) =>
   {
     setBudgets([...budget,newbudget])
+    Alert.alert("Budget Sucessfully Added")
+    
   }
 
   //delete budget and edit budget
@@ -66,6 +68,17 @@ export default function App()
     setBudgets(updatedBudgets);
   }
 
+
+  function AddBudgetStackScreen() { 
+    const AddBudgetStack = createNativeStackNavigator();
+    return (
+      <AddBudgetStack.Navigator>
+
+                <AddBudgetStack.Screen name = 'Budget' children ={()=><BudgetTab  budgets={budget} addBudget={handleAddBudget} delBudget={deleteBudget}/>}/>
+                <AddBudgetStack.Screen name='AddBudget' options={{title: 'Add a Budget'}} children ={()=><AddBudgetTab   addBudget={handleAddBudget}/>} />
+      </AddBudgetStack.Navigator>
+    );
+  }
 
 
 
@@ -113,7 +126,7 @@ export default function App()
             })}            
           >
               <Tab.Screen name = 'Home' children ={()=><HomeTab  budgets={budget}/>}/>
-              <Tab.Screen name = 'Budget' children ={()=><BudgetTab  budgets={budget} addBudget={handleAddBudget} delBudget={deleteBudget}/>}/>
+              <Tab.Screen name = 'Budget' options={{headerShown: false, unmountOnBlur: true}} component={AddBudgetStackScreen}/>
               <Tab.Screen name='Expense' component = {ExpenseTab}/>
               <Tab.Screen name='Settings' component = {SettingsTab}/>
           </Tab.Navigator>
